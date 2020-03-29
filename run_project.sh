7a01e2acd1fffa799a372e9ec8d57002fbddbbfe
Aswin Vijayakumar <aswinvk28@gmail.com>
2020-03-29 16:16:32 +0100

    project run script

--- run_project.sh
+++ run_project.sh
@@ -1,9 +1,7 @@
-sudo docker exec investigate sh /opt/autopsy/autopsy-4.14.0/unix_setup.sh
-
 echo -e "Generating Report..\r"
-HEADER="|--|---|--|---|--|--|---|----|----|----|----|"
-PLACEHOLDER="|obj_id|filename|meta_addr|permissions|--|--|size|ctime|atime|--|mtime|"
+PLACEHOLDER="--|---|--|---|--|--|---|----|----|----|----"
+HEADER="|obj_id|filename|meta_addr|permissions|--|--|size|ctime|atime|--|mtime|"
 sudo docker exec investigate tsk_gettimes -i raw /opt/autopsy/images/usb-image.dd > output.md
 
-sed -i '1 a "$PLACEHOLDER"' output.md
-sed -i 'a "$HEADER"' output.md
+echo -e "$HEADER\n$(cat output.md)" > output.md
+sed -i "1 a $PLACEHOLDER" output.md
